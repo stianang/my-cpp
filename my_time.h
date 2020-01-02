01d04998f0359cfed4b83c47507de42dc37fb3b6
Stian Angelsen <sangelsen@gmail.com>
2020-01-02 14:57:34 +0000

    Added duration_since functions

--- my_time.h
+++ my_time.h
@@ -17,6 +17,16 @@ namespace my
                   (std::chrono::system_clock::now().time_since_epoch()).count();
         }
 
+        unsigned long duration_seconds_since(unsigned long start)
+        {
+            return epoch_seconds_now() - start;
+        }
+
+        unsigned long duration_milliseconds_since(unsigned long start)
+        {
+            return epoch_milliseconds_now() - start;
+        }
+
         void sleepForSeconds(uint seconds)
         {
              std::this_thread::sleep_for(std::chrono::seconds(seconds));
